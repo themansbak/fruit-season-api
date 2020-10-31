273a8b5c4b285ce21feb7aae69c9fef489926da2
AMan <man.alex1995@gmail.com>
2020-10-31 16:27:23 -0700

    Update catch

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,8 @@ const app = express();
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true, useUnifiedTopology: true 
 })
-.then(() => console.log('--- DB connected! ---')
-.catch(err => console.log(err)));
+.then(() => console.log('--- DB connected! ---'))
+.catch(err => console.log(err));
 
 app.use(logger);
 /* [Mark]: Added for CORS policy error message */
