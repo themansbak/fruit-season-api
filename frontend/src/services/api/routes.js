1c4ccb399d66663357a83f3c2b635d7ebf7391a1
AMan <man.alex1995@gmail.com>
2020-10-31 22:49:06 -0700

    Frontend routing

--- frontend/src/services/api/routes.js
+++ frontend/src/services/api/routes.js
@@ -1,5 +1,5 @@
 import { getData, getRes } from "./calls";
-export const BASE_URI = `${window.location.protocol}//${window.location.hostname}:3001` || "http://localhost:3001";
+export const BASE_URI = `${window.location.protocol}//${window.location.hostname}:5000` || "http://localhost:5000";
 export const ALL_STATES = `${BASE_URI}/states`;
 export const ALL_SEASONS = `${BASE_URI}/seasons`;
 const seasons_path = "/seasons";
@@ -10,13 +10,16 @@ const states_path = "/states";
 // const extractUrl = `${proxy}/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${title}&formatversion=2&exsentences=10&exlimit=1&explaintext=1`;
 
 export const getSeasons = (setSeasons, setSeason) => {
+  console.log('base uri: ' + BASE_URI);
   getData(BASE_URI + seasons_path, "seasonName", setSeasons, setSeason);
 };
 
 export const getStates = (setStates, setState) => {
+  console.log('base uri: ' + BASE_URI);
   getData(BASE_URI + states_path, "stateName", setStates, setState);
 };
 
 export const getProduce = (setProduce, state, season) => {
+  console.log('base uri: ' + BASE_URI);
   getRes(`${BASE_URI}/${state}/${season}`, "fruits", setProduce);
 };
